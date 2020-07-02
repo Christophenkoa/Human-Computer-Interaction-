3821a2682028f38020ab5628cc9f64667594b394
Nkoa Christophe <nkoachristophe@gmail.com>
2020-07-02 10:07:16 +0100

    implement appLocker

--- app/src/main/java/com/example/bepresent/MainActivity.java
+++ app/src/main/java/com/example/bepresent/MainActivity.java
@@ -7,11 +7,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
